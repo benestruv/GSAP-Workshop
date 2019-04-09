082fbcbeb2d34d158a4188bb160e0d2220c244b3
benestruv <benedicte.struvay@gmail.com>
2019-04-09 10:29:39 +0200

    workshop

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 /* 
 --<>--<>--<>--<>--<>--<>-- TWEENMAX --<>--<>--<>--<>--<>--<>--
-
 Syntaxe de TweenMax :
 TweenMax.to("#target",duration,{vars});
-
 --<>--<>--<>--<>--<>-- .to / .from / .staggerFrom / .staggerTo --<>--<>--<>--<>--<>--<>--
 .to (du résultat CSS vers TweenMax)
 .from (du résultat TweenMax vers CSS)
 .staggerFrom & .staggerTo (Pareil mais avec une animation différée)
-
 --<>--<>--<>--<>--<>-- Propriétés --<>--<>--<>--<>--<>--<>--
 {y:800} OU {x : 600}
 {left : 600}
@@ -22,21 +19,18 @@ TweenMax.to("#target",duration,{vars});
 {ease:Back.easeOut} ou {ease:Elastic.easeOut} etc,...
 https://greensock.com/ease-visualizer
 {delay:0.5}
-
 Exemple :
 TweenMax.from(".class",2,{
     x:600,
     rotation: 360,
     opacity: 1,
 })
-
 Dans le cas d'un stagger, il faut rajouter un interval :
 TweenMax.staggerFrom(".class",2,{
     x:600,
     rotation: 360,
     opacity: 1,
 },0.5)
-
 */
 
 // ------------------------------------------------- SLIDE 1 (Play with Gsap)
@@ -61,42 +55,29 @@ TweenMax.staggerFrom(".class",2,{
 
 /*
 --<>--<>--<>--<>--<>--<>-- TIMELINEMAX --<>--<>--<>--<>--<>--<>--
-
 1. Créer une variable qui contient la timeline
     var MonAnimation = new TimelineMax();
 2.
-
 Ensuite, rajouter vos animation dans cette variable comme ceci :
-
 var anim1 = TweenMax.to...
 var anim2 = TweenMax.to...
 MonAnimation
     .add(anim1)
     .add(anim2)
-
 OU
-
 MonAnimation
     .to(".class",2,{params})
     .from(".class",2,{params})
-
-
-
-
 --<>--<>--<>--<>--<>-- SCROLLMAGIC --<>--<>--<>--<>--<>--<>--
-
 1. On commence par initialiser le controller :
 var controller = new ScrollMagic();
-
 2. Créer une scène
 new ScrollMagic.Scene({
     triggerElement: "#id", // Le point de commencement de l'animation
     triggerHook: "onEnter",// onEnter, onCenter, onLeave
 })
-
     .setTween("#id")// Ajoute une animation gsap
     .addTo(controller) // Ajoute l'animation au controller
     .addIndicators(); // Permet de voir les triggers
     .setPin("#id"); // fixe une div au scroll (effet de volets)
-
 */
